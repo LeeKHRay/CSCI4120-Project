5e421f637e26f4019e57fc191ec4b7f7518323a3
LeeKHRay <49152689+LeeKHRay@users.noreply.github.com>
2021-05-16 18:34:53 +0800

    added collision detection to main camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cam;
+
+    private RaycastHit hit;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = cam.localPosition;
+    }
+
+    void Update()
+    {
+        if (Physics.Linecast(transform.position, transform.position + transform.rotation * offset, out hit))
+        {
+            cam.localPosition = new Vector3(0, 0, -Vector3.Distance(transform.position, hit.point));
+            
+        }
+        else
+        {
+            // move to original position smoothly
+            cam.localPosition = Vector3.Lerp(cam.localPosition, offset, Time.deltaTime);
+        }
+    }
+}
