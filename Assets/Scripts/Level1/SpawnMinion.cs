ac5b419c9d670f6f7733dd23046d4d5922fe12a3
LeeKHRay <49152689+LeeKHRay@users.noreply.github.com>
2021-05-20 17:03:31 +0800

    add SpawnMinion

---
+++ Assets/Scripts/Level1/SpawnMinion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMinion : MonoBehaviour
+{
+    public Transform minionSpawnPoint;
+    public GameObject minion;
+
+    private ParticleSystem[] particleSystems;
+    private bool spawned = false;
+
+
+    void Start()
+    {
+        particleSystems = minionSpawnPoint.GetComponentsInChildren<ParticleSystem>();
+    }
+
+    private IEnumerator Spawn()
+    {
+        particleSystems[0].Play();
+        particleSystems[1].Play();
+        yield return new WaitForSeconds(2.0f);
+        minion.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!spawned && other.tag == "Player")
+        {
+            spawned = true;
+            StartCoroutine("Spawn");
+        }
+    }
+}
